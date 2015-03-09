d7e4499c133edfab63655230a771caaf46a5f90b
Marc Unterseh <148278@supinfo.com>
2015-03-09 14:30:20 +0100

    Tp 2 MVC

---
+++ TP2MVC/TP2MVC/Controllers/StudentController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TP2MVC.Models;
+
+namespace TP2MVC.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Detail()
+        {
+            var student = new Student
+            {
+                Name = "Toto", 
+                Class = 3, 
+                Email = "toto@toto.fr", 
+                JoinDate = DateTime.Now, 
+                HasPaid = true
+            };
+
+            return View(student);
+        }
+    }
+}
---
+++ TP2MVC/TP2MVC/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TP2MVC.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int Class { get; set; }
+        public DateTime JoinDate { get; set; }
+        public bool HasPaid { get; set; }
+
+    }
+}
