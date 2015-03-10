c86ba6602c34e609a4b471849a41e7291106c961
Marc Unterseh <148278@supinfo.com>
2015-03-10 17:54:36 +0100

    tp 6

---
+++ TP4MVCAnnotation/TP2MVC/Controllers/Student2Controller.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TP2MVC.Models;
+using TP2MVC.ViewModels;
+
+namespace TP2MVC.Controllers
+{
+    public class Student2Controller : Controller
+    {
+        // GET: Student2
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(CreateViewModel vm)
+        {
+            if (ModelState.IsValid)
+            {
+                using (var context = new SchoolContext())
+                {
+                    context.Students.Add(vm.Student);
+                    context.SaveChanges();
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ TP4MVCAnnotation/TP2MVC/ViewModels/CreateViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TP2MVC.Models;
+
+namespace TP2MVC.ViewModels
+{
+    public class CreateViewModel
+    {
+        public Student Student { get; set; }
+    }
+}
